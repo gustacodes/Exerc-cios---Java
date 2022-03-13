38d19f46cdaea9b504de9ba87405d83a1ba667ff
Gustavo Henrique <86691048+gustacodes@users.noreply.github.com>
2022-03-13 00:06:17 -0300

    Add files via upload

---
+++ Salario.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/*
+Crie um algoritmo que leia o valor do salário mínimo e o valor do salário de um usuário,
+calcule a quantidade de salários mínimos esse usuário ganha e imprima o resultado.
+(1SM=R$788,00)
+*/
+
+public class Salario {
+    public static void main(String[] args) {
+
+        System.out.println("Informe seu sakário:");
+        Scanner sc = new Scanner(System.in);
+        int salario = sc.nextInt();
+
+        System.out.println(salario / 788);
+            
+    }
+}
